2c52ec1702041c046e98eac2d6c7c9b5e0f6ae61
Paweł Matysiak <mat.pawel@gmail.com>
2020-11-29 18:25:12 +0100

    Prepare Aplication for Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
@@ -15,6 +16,12 @@ app.use('/api', seatsRoutes);
 
 
 
-app.listen(8000, () => {
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
